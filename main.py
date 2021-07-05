56d119088e9d23ca5ffe306fdb420500f97bdc2f
marcos7junior <33362130+marcos7junior@users.noreply.github.com>
2021-07-05 09:09:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
+# imports
 import cv2
 
-rtmp = "rtmp://192.168.0.104:1935/"
+# rtmp://IP:PORTA/
+rtmp = "rtmp://0.0.0.0:1935/"
 
+# captura o vídeo transmitido via RTMP
 cap = cv2.VideoCapture(rtmp)
 
 while True:
+    # recebe um valor lógico(success) e uma matriz com os frames
     success, frame = cap.read()
-
+    
+    # converte para escala cinza
     capGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    # redimensiona ambas as janelas
     capGray = cv2.resize(capGray, (640, 400))
     frame = cv2.resize(frame, (640, 400))
 
     if success:
+        # se o valor lógico for verdadeiro irá mostrar os 
+        # frames até a tecla 'ESC' for presionada
         cv2.imshow("Mavic", frame)
         cv2.imshow("Gray Scale", capGray)
         key = cv2.waitKey(1)
